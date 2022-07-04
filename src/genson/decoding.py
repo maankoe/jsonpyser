f0095ab4744748ad1f84a2c4bbb50ff9f30b3786
maanrit <108694058+maanrit@users.noreply.github.com>
2022-07-04 20:52:23 +0100

    Changed implementation and added list parsing

--- src/genson/decoding.py
+++ src/genson/decoding.py
@@ -11,33 +11,75 @@ def is_int_match(x):
 def is_float_match(x):
     return float_re.match(x)
 
-def decode_int(number):
-    return int(number)
+def decode_int(number_json):
+    return int(number_json)
 
-def decode_float(number):
-    return float(number)
+def decode_float(number_json):
+    return float(number_json)
 
-def decode_string(string):
-    return string[1:-1]
+def decode_string(string_json):
+    return string_json[1:-1]
 
-# def array_end_index(array):
-#     return array.index("]")
 
-def decode_array(array):
-    inner = decode_json(array[1:-1])
-    if inner is not None:
-        return [inner]
+def decode_item(item_json):
+    if len(item_json) == 0:
+        return None
+    elif is_int_match(item_json):
+        return decode_int(item_json)
+    elif is_float_match(item_json):
+        return decode_float(item_json)
     else:
-        return []
+        return decode_string(item_json)
 
-def decode_json(json_string):
+def item_json(context):
+    item_json = "".join(context)
+    return item_json.strip()
+
+
+class JSONDecoder():
+    def __init__(self):
+        self.context = []
+        self.output = None
+        self.pos = 0
+
+    def decode_context(self):
+        context_json = item_json(self.context)
+        if len(context_json) > 0:
+            self.output.append(decode_item(context_json))
+            self.context = []
+
+    def decode_array(self, json_string):
+        decoder = JSONDecoder()
+        data = decoder.decode(json_string)
+        self.pos += decoder.pos + 1
+        self.output.append(data)
+
+    def initialize_output(self, start_char):
+        if start_char == "[":
+            self.output = []
+        self.pos += 1
+
+    def decode(self, json_string):
+        self.initialize_output(json_string[0])
+        while self.pos < len(json_string):
+            x = json_string[self.pos]
+            if x == ",":
+                self.decode_context()
+            elif x == "[":
+                self.decode_array(json_string[self.pos:])
+            elif x == "]":
+                self.decode_context()
+                break
+            else:
+                self.context.append(x)
+            self.pos += 1
+        return self.output
+
+def decode_json(json_string: str):
+    json_string = json_string.lstrip()
     if len(json_string) == 0:
         return None
+    elif json_string[0] != "[":
+        return decode_item(json_string.strip())
     elif json_string[0] == "[":
-        return decode_array(json_string)
-    elif is_int_match(json_string):
-        return decode_int(json_string)
-    elif is_float_match(json_string):
-        return decode_float(json_string)
-    else:
-        return decode_string(json_string)
+        return JSONDecoder().decode(json_string)
--- test/test_genson/test_basic_decoding.py
+++ test/test_genson/test_basic_decoding.py
@@ -4,24 +4,40 @@ from genson.decoding import decode_json
 
 class TestBasicDecoding(unittest.TestCase):
     def test_decode_empty_string(self):
-        self.assertEquals(decode_json('""'), "")
+        self.assertEqual(decode_json('""'), "")
 
     def test_decode_string(self):
-        self.assertEquals(decode_json('"ASDF"'), "ASDF")
+        self.assertEqual(decode_json('"ASDF"'), "ASDF")
 
     def test_decode_int(self):
-        self.assertEquals(decode_json("1"), 1)
-        self.assertEquals(decode_json("13"), 13)
-        self.assertEquals(decode_json("123"), 123)
+        self.assertEqual(decode_json("1"), 1)
+        self.assertEqual(decode_json("1 "), 1)
+        self.assertEqual(decode_json(" 13"), 13)
+        self.assertEqual(decode_json("123"), 123)
 
     def test_decode_float(self):
-        self.assertEquals(decode_json("1.4"), 1.4)
-        self.assertEquals(decode_json("10.4"), 10.4)
-        self.assertEquals(decode_json("12.45"), 12.45)
-        self.assertEquals(decode_json("12."), 12.)
+        self.assertEqual(decode_json("1.4"), 1.4)
+        self.assertEqual(decode_json("10.4 "), 10.4)
+        self.assertEqual(decode_json(" 12.45"), 12.45)
+        self.assertEqual(decode_json("12."), 12.)
 
     def test_decode_empty_list(self):
         self.assertEqual(decode_json("[]"), [])
+        self.assertEqual(decode_json(" []"), [])
+        self.assertEqual(decode_json("[] "), [])
+        self.assertEqual(decode_json("[ ] "), [])
+        self.assertEqual(decode_json(" [  ] "), [])
 
     def test_decode_list_with_int(self):
         self.assertEqual(decode_json("[1]"), [1])
+
+    def test_decode_list_with_multiple_ints(self):
+        self.assertEqual(decode_json("[1, 2, 3]"), [1, 2, 3])
+        self.assertEqual(decode_json("[1,2,3]"), [1, 2, 3])
+        self.assertEqual(decode_json("[1,2 , 3]"), [1, 2, 3])
+
+    def test_decode_nested_int_list(self):
+        self.assertEqual(decode_json("[[1, 2], [3, 4], 5]"), [[1, 2], [3, 4], 5])
+
+    def test_decode_double_nested_int_list(self):
+        self.assertEqual(decode_json("[[1, [2, 3], 4], [3, 4], 5]"), [[1, [2, 3], 4], [3, 4], 5])
